5fa50882a3ac6a61a53d4fe09e88639201ff8455
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-12-11 10:39:57 +0630

    Update index.jsx

--- src/app/pages/addToCart/index.jsx
+++ src/app/pages/addToCart/index.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const MyCart = () => {
   return (
-    <div>MyCart</div>
+    <section className="bg-gray-200 flex flex-col items-center justify-center min-h-screen">
+
+     <h2 className="card-title text-xl">My Cart</h2>
+
+   
+  </section>
   )
 }
 
