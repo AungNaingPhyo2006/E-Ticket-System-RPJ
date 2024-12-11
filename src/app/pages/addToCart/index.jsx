7a292306708de915cccaf6a0c1e950c12f28a59c
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-12-11 11:02:49 +0630

    Update index.jsx

--- src/app/pages/addToCart/index.jsx
+++ src/app/pages/addToCart/index.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const MyCart = () => {
   return (
-    <section className="bg-gray-200 flex flex-col items-center justify-center min-h-screen">
-
-     <h2 className="card-title text-xl">My Cart</h2>
-
-   
-  </section>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">My Cart</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
